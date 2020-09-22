1321cfee2dee833fafa6293c03327445316c492d
O Rabie <o.rabie@tap.company>
2020-09-22 12:55:29 +0200

    Upgrade to Xcode12+ compiler friendly code

---
+++ TapNetworkManagerV2/Pods/Target Support Files/Pods-TapNetworkManagerV2/Pods-TapNetworkManagerV2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TapNetworkManagerV2 : NSObject
+@end
+@implementation PodsDummy_Pods_TapNetworkManagerV2
+@end
---
+++ TapNetworkManagerV2/TapNetworkManagerV2/TapNetworkManagerV2.h
@@ -0,0 +1,18 @@
+//
+//  TapNetworkManagerV2.h
+//  TapNetworkManagerV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapNetworkManagerV2.
+FOUNDATION_EXPORT double TapNetworkManagerV2VersionNumber;
+
+//! Project version string for TapNetworkManagerV2.
+FOUNDATION_EXPORT const unsigned char TapNetworkManagerV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapNetworkManagerV2/PublicHeader.h>
+
+
